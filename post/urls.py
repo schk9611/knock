e56fdf5e7ab4fd64901688ca31da4020b3eb67a7
codebot053 <codebot053@gmail.com>
2022-12-13 22:48:12 +0900

    Revert "Refactor: GenericPostModelViewSet router 추가"
    This reverts commit 87f8175e6f38ddc175d3cfdf6d541c54e8f61ac5.

--- post/urls.py
+++ post/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register("", views.GenericPostModelViewSet)
+router.register("post", views.PostModelViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
