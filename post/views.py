103d60a72baaebb337098986b15726545604f326
codebot053 <codebot053@gmail.com>
2022-12-13 21:41:21 +0900

    Feat: GenericPostModelViewSet 추가 - ModelViewSet을 상속하여 Post모델의 CRRUD 기본구조 구현

--- post/views.py
+++ post/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from .models import Post
+from .serializers import GenericPostSerializer
+class GenericPostModelViewSet(ModelViewSet):
+    
+    queryset = Post.objects.all()
+    serializer_class = GenericPostSerializer
+    
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        '''
+        is_public 필드값이 True인 글들만 리스트 합니다.
+        '''
+        qs = self.queryset.filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
 
-# Create your views here.
+    def perform_create(self, serializer):
+        '''
+        해당 유저정보를 form, json을 통하지 않고 request 통해 저장
+        '''
+        serializer.save(author=self.request.user)
