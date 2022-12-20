eac7bbe9f42abb766e733e5bcf5bbda2f40cf930
gyuchan <schk9611@gmail.com>
2022-12-20 23:58:12 +0900

    Feat(user/urls.py): 소셜로그인(구글) 관련 url 작성

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from .views import UserViewSet
+from .views import UserViewSet, google_login, google_callback, GoogleLogin
 
 
 router = routers.DefaultRouter()
@@ -9,4 +9,7 @@ router.register("user", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("google/login", google_login, name="google_login"),
+    path("google/callback/", google_callback, name="google_callback"),
+    path("google/login/finish/", GoogleLogin.as_view(), name="google_login_todjango"),
 ]
