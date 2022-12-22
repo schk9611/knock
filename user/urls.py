187ad809c9c0734d42cd9992361e465946de700e
gyuchan <schk9611@gmail.com>
2022-12-22 15:58:23 +0900

    Feat(user/urls.py): 카카오 소셜로그인 기능 구현을 위한 url 설정

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,15 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from .views import UserViewSet, google_login, google_callback, GoogleLogin
+from .views import (
+    UserViewSet,
+    google_login,
+    google_callback,
+    GoogleLogin,
+    kakao_login,
+    kakao_callback,
+    KakaoLogin,
+)
 
 
 router = routers.DefaultRouter()
@@ -9,7 +17,12 @@ router.register("", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    # 구글 소셜 로그인
     path("google/login", google_login, name="google_login"),
     path("google/callback/", google_callback, name="google_callback"),
     path("google/login/finish/", GoogleLogin.as_view(), name="google_login_todjango"),
+    # 카카오 소셜 로그인
+    path("kakao/login/", kakao_login, name="kakao_login"),
+    path("kakao/callback/", kakao_callback, name="kakao_callback"),
+    path("kakao/login/finish/", KakaoLogin.as_view(), name="kakao_login_todjango"),
 ]
