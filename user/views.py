969729929abd8d7b5cfc27bb7f26ae9fe6dbd15d
gyuchan <schk9611@gmail.com>
2022-12-23 14:58:37 +0900

    Refactor: kakao_callback 함수의 주석 수정 및 SocialAccount.DoesNotExist에 대한 except 추가

--- user/views.py
+++ user/views.py
@@ -137,10 +137,10 @@ def kakao_callback(request):
     token_req = requests.get(
         f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={rest_api_key}&redirect_uri={redirect_uri}&code={code}"
     )
-    token_req_json = token_req.json()
 
-    # 에러 발생 시 중단
+    token_req_json = token_req.json()
     error = token_req_json.get("error")
+
     if error is not None:
         raise JSONDecodeError(error)
 
@@ -150,8 +150,8 @@ def kakao_callback(request):
     profile_request = requests.post(
         "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"}
     )
-    profile_json = profile_request.json()
 
+    profile_json = profile_request.json()
     kakao_account = profile_json.get("kakao_account")
     email = kakao_account.get("email")
 
@@ -161,17 +161,20 @@ def kakao_callback(request):
 
     try:
         user = User.objects.get(email=email)
-        # 기존에 가입된 유저의 Provider가 kakao가 아니면 에러 발생, 맞으면 로그인
+
         social_user = SocialAccount.objects.get(user=user)
+
         if social_user is None:
             return JsonResponse(
                 {"err_msg": "email exists but not social user"}, status=status.HTTP_400_BAD_REQUEST
             )
+
         if social_user.provider != "kakao":
             return JsonResponse(
                 {"err_msg": "no matching social type"}, status=status.HTTP_400_BAD_REQUEST
             )
-        # 기존에 Google로 가입된 유저
+
+        # 이미 kakao로 회원가입된 유저면 로그인 & 해당 유저의 jwt 발급
         data = {"access_token": access_token, "code": code}
         accept = requests.post(f"{BASE_URL}user/kakao/login/finish/", data=data)
         accept_status = accept.status_code
@@ -184,7 +187,6 @@ def kakao_callback(request):
         return JsonResponse(accept_json)
 
     except User.DoesNotExist:
-        # 기존에 가입된 유저가 없으면 새로 가입
         data = {"access_token": access_token, "code": code}
         accept = requests.post(f"{BASE_URL}user/kakao/login/finish/", data=data)
         accept_status = accept.status_code
@@ -192,10 +194,14 @@ def kakao_callback(request):
         if accept_status != 200:
             return JsonResponse({"err_msg": "failed to signup"}, status=accept_status)
 
-        # user의 pk, email, first name, last name과 Access Token, Refresh token 가져옴
         accept_json = accept.json()
         accept_json.pop("user", None)
         return JsonResponse(accept_json)
+
+    except SocialAccount.DoesNotExist:
+        return JsonResponse(
+            {"err_msg": "email exists but not social user"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class GoogleLogin(SocialLoginView):
