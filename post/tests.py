bdc4fdc6fb3c52aef76e6b3adc00e0e646d5bcaf
codebot053 <codebot053@gmail.com>
2022-12-24 23:46:57 +0900

    Init: post/tests.py GenericPostModelViewSetTest 생성

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+from django.urls import reverse
+from django.conf import settings
+
+
+class GenericPostModelViewSetTest(APITestCase):
+    """post/views.py 의 GenericPostModelViewSet 이 정상적으로 동작하는지를 검증하는 테스트"""
+
+    pass
